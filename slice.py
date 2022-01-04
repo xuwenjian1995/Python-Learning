231a098098ed4fce53a05f5e9ce1c70873e5baa4
wenjian <1214703195@qq.com>
2022-01-04 20:12:37 +0800

    add unittest

--- slice.py
+++ slice.py
@@ -1,33 +1,11 @@
-#闭包和装饰器
-from functools import wraps
+class Dict(dict):
+	def __init__(self,**kw):
+		super().__init__(**kw)
 
-def add_infor(func1):
-	@wraps(func1)
-	def wrapper():
-
-		print("Below is my name:")
-		func1()
-	return wrapper
-
-@add_infor
-def get_name():
-	print("wenjian")
-
-#get_name()
-#print(get_name.__name__)
-
-
-class Student(object):
-	#__slots__=("name","world")
-	def __init__(self, name):
-		self.name = name
-
-stu = Student("lilei")
-print(stu.name)
-stu.test = 1
-
-# print(stu.name)
-# print(stu.__name)
-#print(stu._Student__name)
-
-
+	def __getattr__(self,key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute''%s'"%key)
+	def __setattr__(self,key,value):
+		self[key] = value
